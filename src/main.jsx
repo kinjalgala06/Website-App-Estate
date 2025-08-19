3d1cd274b95812edc8f908f718e1e51114f31a7d
kinjalgala06 <kinjalgala2004@gmail.com>
2025-08-19 18:39:02 +0530

    initial upload

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <StrictMode basename={import.meta.env.BASE_URL}>
     <App />
   </StrictMode>,
 )
